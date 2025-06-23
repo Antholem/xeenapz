004483fb1e603aef64dd579593837cf6425c17e8
Antholem <antholemlemmanalo@gmail.com>
2025-06-23 12:16:25 +0800

    Restructured stores folder

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -1,7 +1,7 @@
-export { default as useAuth } from "./useAuth";
-export { default as useMessageInputPersistent } from "./useMessageInputPersistent";
-export { default as useMessagePersistent } from "./useMessagePersistent";
-export { default as useTempChat } from "./useTempChat";
-export { default as useTheme } from "./useTheme";
+export { default as useAuth } from "./auth/useAuth";
+export { default as useMessageInputPersistent } from "./chat/useMessageInputPersistent";
+export { default as useMessagePersistent } from "./chat/useMessagePersistent";
+export { default as useTempChat } from "./chat/useTempChat";
+export { default as useTheme } from "./theme/useTheme";
 
-export type { Message } from "./useMessagePersistent";
+export type { Message } from "./chat/useMessagePersistent";
