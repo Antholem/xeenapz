5681ead8b26421f417f6d7ccf01f278419755545
Sam Antholem Manalo <83632082+Antholem@users.noreply.github.com>
2025-08-16 13:36:58 +0800

    feat: implement custom image overlay

--- src/components/ui/ImageModal/index.tsx
+++ src/components/ui/ImageModal/index.tsx
@@ -1,45 +1,51 @@
 "use client";
 
-import { FC } from "react";
-import {
-  Modal,
-  ModalOverlay,
-  ModalCloseButton,
-  ModalBody,
-  type ModalProps,
-  Image,
-} from "@chakra-ui/react";
-import ModalContent from "../ModalContent";
+import { FC, useEffect } from "react";
+import { Box, Image, CloseButton } from "@chakra-ui/react";
 
-interface ImageModalProps extends Omit<ModalProps, "children"> {
+interface ImageModalProps {
   src: string;
   alt: string;
+  isOpen: boolean;
+  onClose: () => void;
 }
 
-const ImageModal: FC<ImageModalProps> = ({ src, alt, ...props }) => {
+const ImageModal: FC<ImageModalProps> = ({ src, alt, isOpen, onClose }) => {
+  useEffect(() => {
+    if (isOpen) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "";
+    }
+    return () => {
+      document.body.style.overflow = "";
+    };
+  }, [isOpen]);
+
+  if (!isOpen) return null;
+
   return (
-    <Modal
-      isCentered
-      size="xl"
-      motionPreset="none"
-      closeOnOverlayClick={false}
-      {...props}
+    <Box
+      position="fixed"
+      inset={0}
+      bgColor="background"
+      display="flex"
+      alignItems="center"
+      justifyContent="center"
+      zIndex="modal"
     >
-      <ModalOverlay bgColor="background" />
-      <ModalContent>
-        <ModalCloseButton
-          position="fixed"
-          top={4}
-          right={4}
-          borderRadius="full"
-          color="primaryText"
-        />
-        <ModalBody p={0}>
-          <Image src={src} alt={alt} w="100%" maxH="80vh" objectFit="contain" />
-        </ModalBody>
-      </ModalContent>
-    </Modal>
+      <CloseButton
+        position="fixed"
+        top={4}
+        right={4}
+        borderRadius="full"
+        color="primaryText"
+        onClick={onClose}
+      />
+      <Image src={src} alt={alt} w="100%" maxH="80vh" objectFit="contain" />
+    </Box>
   );
 };
 
 export default ImageModal;
+
