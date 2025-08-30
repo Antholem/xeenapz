20ab40890d0c594cf876141b57d365ebc6e32b59
Sam Antholem Manalo <83632082+Antholem@users.noreply.github.com>
2025-08-30 22:06:17 +0800

    fix: apply accent color to select options

--- src/components/ui/Select/index.tsx
+++ src/components/ui/Select/index.tsx
@@ -15,12 +15,26 @@ const Select = forwardRef<HTMLSelectElement, SelectProps>(
     const computedFocusBorderColor =
       focusBorderColor ??
       (colorMode === "dark" ? `${accentColor}.300` : `${accentColor}.400`);
+    const optionBg = colorMode === "dark" ? "gray.700" : "white";
+    const optionHighlightBg =
+      colorMode === "dark" ? `${accentColor}.600` : `${accentColor}.200`;
+    const optionHighlightColor =
+      colorMode === "dark" ? "white" : "gray.800";
 
     return (
       <ChakraSelect
         ref={ref}
         colorScheme={accentColor}
         focusBorderColor={computedFocusBorderColor}
+        sx={{
+          option: {
+            backgroundColor: optionBg,
+          },
+          "option:checked, option:hover": {
+            backgroundColor: optionHighlightBg,
+            color: optionHighlightColor,
+          },
+        }}
         {...props}
       />
     );
