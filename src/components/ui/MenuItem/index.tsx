93f701d04fa3d137e50fa5167c1935ead31e2eba
Sam Antholem Manalo <83632082+Antholem@users.noreply.github.com>
2025-08-31 22:42:21 +0800

    refactor: remove redundant menu item state styling

--- src/components/ui/MenuItem/index.tsx
+++ src/components/ui/MenuItem/index.tsx
@@ -4,14 +4,7 @@ import { forwardRef } from "react";
 import { MenuItem as ChakraMenuItem, type MenuItemProps } from "@chakra-ui/react";
 
 const MenuItem = forwardRef<HTMLDivElement, MenuItemProps>((props, ref) => (
-  <ChakraMenuItem
-    ref={ref}
-    bgColor="transparent"
-    _hover={{ bgColor: "transparent" }}
-    _active={{ bgColor: "transparent" }}
-    _focus={{ bgColor: "transparent" }}
-    {...props}
-  />
+  <ChakraMenuItem ref={ref} bgColor="transparent" {...props} />
 ));
 
 MenuItem.displayName = "MenuItem";
